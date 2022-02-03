1b579b911b5f544c0ec189814a38ee8b2a923a72
fidashn <87206839+fidashn@users.noreply.github.com>
2022-02-03 02:58:40 +0200

    bug check

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,6 @@
 import './Projects.css';
 import ImageSlider from './Slider';
-import { SliderData } from './SlideData';
+import { SliderData } from './SlideData.jsx';
 
 const Projects = () => {
   return <ImageSlider slides={SliderData} />;
