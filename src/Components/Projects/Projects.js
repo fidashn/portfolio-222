15bd0a5fe657d5f97df462ac3bb884e695e05ecf
fidashn <87206839+fidashn@users.noreply.github.com>
2022-02-03 09:32:01 +0200

    checking path

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,9 +1,12 @@
 import './Projects.css';
-import ImageSlider from './Slider';
-import { SliderData } from './SlideData.jsx';
+// import ImageSlider from './Slider';
+// import { SliderData } from './SlideData.jsx';
 
 const Projects = () => {
-  return <ImageSlider slides={SliderData} />;
+  return (
+    <div>hello</div>
+  )
+  //  <ImageSlider slides={SliderData} />;
 
 
 };
