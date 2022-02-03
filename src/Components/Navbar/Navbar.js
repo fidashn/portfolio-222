da879a87c7d8b15001247f9d77049705b6025ca7
fidashn <87206839+fidashn@users.noreply.github.com>
2022-02-03 12:28:35 +0200

    change a tags to react-router-dom links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -8,8 +9,8 @@ const Navbar = () => {
       <span class="name">Fida Shnakher<i class="far fa-chess-queen"></i></span>
       <ul className='ul1'>
         <a href="FIDA-SHNAKHER-22.pdf"><button class="download">Download cv</button></a>
-        <li><a href="/">Home</a></li>
-        <li><a href="projects">Projects</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="projects">Projects</Link></li>
       </ul>
       </nav>
     </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,8 +4,6 @@ import { SliderData } from './SlideData.jsx';
 
 const Projects = () => {
   return <ImageSlider slides={SliderData} />;
-
-
 };
 
 export default Projects;
