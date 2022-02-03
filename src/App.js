e916f0bb2571d16bf44d5a643e30d04607cc1528
fidashn <87206839+fidashn@users.noreply.github.com>
2022-02-03 12:32:28 +0200

    lift up BrowserRouter into index.js file

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import Nav from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Projects from './Components/Projects/Projects';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
 
     <div className="App">
       <Nav />
-      <Router>
-        <Switch>
-          <Route exact path="/"render={() =><Home/>} />
-          <Route exact path="/projects" render={() =><Projects/>} />
-        </Switch >
-      </Router>
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/projects" render={() => <Projects />} />
+      </Switch >
       <Footer />
     </div>
 
