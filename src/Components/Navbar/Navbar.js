ba607e22fea0bc401c650db7c1f27491b3477b46
fidashn <87206839+fidashn@users.noreply.github.com>
2022-05-11 16:06:23 +0300

    updated cv

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <nav className='nav'>
       <span class="name">Fida Shnakher<i class="far fa-chess-queen"></i></span>
       <ul className='ul1'>
-        <a href="FIDA-SHNAKHER-222.pdf"><button class="download">Download cv</button></a>
+        <a href="FIDA--SHNAKHER-FE.pdf"><button class="download">Download cv</button></a>
         <li><Link to="/">Home</Link></li>
         <li><Link to="projects">Projects</Link></li>
       </ul>
